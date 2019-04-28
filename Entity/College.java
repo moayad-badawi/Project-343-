2c94d09422fc649636a1fbc40841462720c7b9de
Jack <jack.du.2013@gmail.com>
2019-04-28 02:47:37 -0700

    Delete College.java

--- Entity/College.java
+++
@@ -1,8 +0,0 @@
-package Entity;
-
-import java.util.ArrayList;
-
-public class College {
-    private String name; // college name
-    private ArrayList<Department> departments; // departments that belong to the college
-}
