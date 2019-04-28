a8917a66ae422f30c693b93883b52c32dc5e9e88
Jack <jack.du.2013@gmail.com>
2019-04-28 02:47:42 -0700

    Delete Course.java

--- Entity/Course.java
+++
@@ -1,9 +0,0 @@
-package Entity;
-
-import java.util.ArrayList;
-
-public class Course {
-    private String ID; // course id. String or int?
-    private String name; // course name
-    private ArrayList<Course> prerequisite; // prereq of the course
-}
