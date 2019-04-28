056e1bc9c45d1d8af83c493974adf41648cab217
Jack <jack.du.2013@gmail.com>
2019-04-28 02:47:51 -0700

    Delete Faculty.java

--- Entity/Faculty.java
+++
@@ -1,9 +0,0 @@
-package Entity;
-
-import java.util.ArrayList;
-
-public class Faculty {
-    private String ID; // string or int
-    private String title;
-    private ArrayList<Session> sessions; // sessions that the faculty teaches
-}
