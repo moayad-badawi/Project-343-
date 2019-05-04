d9eeea5034acdc1221301eec63709d714230dcbf
yurikong <jack.du.2013@gmail.com>
2019-05-03 20:14:44 -0700

    give middle name, for middle initial use substring

--- datatypes/Admin.java
+++ datatypes/Admin.java
@@ -4,20 +4,20 @@ public class Admin
 {
 	private final int id;
 	private String firstname;
-	private char middleInitial;
+	private String middlename;
 	private String lastname;
-	public Admin(final int id, String firstname, char middleInitial, String lastname)
+	public Admin(final int id, String firstname, String middlename, String lastname)
 	{
 		this.id = id;
 		this.firstname = firstname;
-		this.middleInitial = middleInitial;
+		this.middlename = middlename;
 		this.lastname = lastname;
 	}
 	public void setFirstname(String firstname) { this.firstname = firstname; }
-	public void setMiddleInitial(char middleInitial) { this.middleInitial = middleInitial; }
+	public void setMiddlename(String middlename) { this.middlename = middlename; }
 	public void setLastname(String lastname) { this.lastname = lastname; }
 	public final int id() { return id; }
 	public String firstname() { return firstname; }
-	public char middleInitial() { return middleInitial; }
+	public String middlename() { return middlename; }
 	public String lastname() { return lastname; }
 }
--- datatypes/Employee.java
+++ datatypes/Employee.java
@@ -6,30 +6,30 @@ public class Employee
 {
 	private final int id;
 	private String firstname;
-	private char middleInitial;
+	private String middlename;
 	private String lastname;
 	private String title;
 	private float salary;
 	private ArrayList<Integer> sessionsTeaching;
-	public Employee(final int id, String firstname, char middleInitial, String lastname, String title, float salary, ArrayList<Integer> sessionsTeaching)
+	public Employee(final int id, String firstname, String middlename, String lastname, String title, float salary, ArrayList<Integer> sessionsTeaching)
 	{
 		this.id = id;
 		this.firstname = firstname;
-		this.middleInitial = middleInitial;
+		this.middlename = middlename;
 		this.lastname = lastname;
 		this.title = title;
 		this.salary = salary;
 		this.sessionsTeaching = sessionsTeaching;
 	}
 	public void setFirstname(String firstname) { this.firstname = firstname; }
-	public void setMiddleInitial(char middleInitial) { this.middleInitial = middleInitial; }
+	public void setMiddlename(String middlename) { this.middlename = middlename; }
 	public void setLastname(String lastname) { this.lastname = lastname; }
 	public void setTitle(String title) { this.title = title; }
 	public void setSalary(float salary) { this.salary = salary; }
 	public void setSessionsTeaching(ArrayList<Integer> sessionsTeaching) { this.sessionsTeaching = sessionsTeaching; }
 	public final int id() { return id; }
 	public String firstname() { return firstname; }
-	public char middleInitial() { return middleInitial; }
+	public String middlename() { return middlename; }
 	public String lastname() { return lastname; }
 	public String title() { return title; }
 	public float salary() { return salary; }
